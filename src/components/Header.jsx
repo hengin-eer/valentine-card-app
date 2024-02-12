62285263c84e7aa63294bef6fd5cdc28237e37f2
hengin-eer <hirokix0608@gmail.com>
2024-02-13 04:19:12 +0900

    ✨feat: header componentを作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export const Header = () => {
+    const MY_X_LINK = "https://x.com/tim_daik"
+    const MY_GITHUB_LINK = "https://github.com/hengin-eer"
+
+    return (
+        <header className="w-full h-[80px] px-8 flex items-center justify-between text-white bg-p-200">
+            <h1 className="text-xl">バレンタインカード アプリ</h1>
+            <ul className="flex items-center gap-5">
+                <Link href={MY_X_LINK}>
+                    <li>🐦My X</li>
+                </Link>
+                
+                <Link href={MY_GITHUB_LINK}>
+                    <li>🐙My GitHub</li>
+                </Link>
+            </ul>
+        </header>
+    )
+}
