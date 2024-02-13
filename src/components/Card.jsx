513d07a85f0f225aceaffd9a646325e2513d4a37
hengin-eer <hirokix0608@gmail.com>
2024-02-13 23:42:28 +0900

    ✨feat: カード部分のUIを作成

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import { CardBody } from "./CardBody";
+import { CardShareButton } from "./CardShareButton";
+
+export const Card = () => {
+
+    return (
+        <div>
+            <CardBody />
+            <CardShareButton />
+        </div>
+    )
+}
