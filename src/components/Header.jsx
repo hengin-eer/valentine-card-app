54a210b9308782424a607c3e2cf5dbfe73c93690
hengin-eer <hirokix0608@gmail.com>
2024-02-14 04:05:47 +0900

    🐛fix: ヘッダーのSNSリンクをアイコンに変更
    TwitterとGitHubだけ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { FaXTwitter, FaGithub } from "react-icons/fa6";
 
 export const Header = () => {
     const MY_X_LINK = "https://x.com/tim_daik"
@@ -9,13 +10,13 @@ export const Header = () => {
             <h1 className="text-xl underline decoration-white decoration-1 underline-offset-4">
                 <Link href="/">Happy Valentine's Card</Link>
             </h1>
-            <ul className="flex items-center gap-5">
+            <ul className="flex items-center gap-5 text-2xl">
                 <Link href={MY_X_LINK}>
-                    <li>🐦My X</li>
+                    <li><FaXTwitter /></li>
                 </Link>
                 
                 <Link href={MY_GITHUB_LINK}>
-                    <li>🐙My GitHub</li>
+                    <li><FaGithub /></li>
                 </Link>
             </ul>
         </header>
