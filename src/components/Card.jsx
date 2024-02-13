4b469ddb1c9568d3cfd691e8d53804dd81f9cc93
hengin-eer <hirokix0608@gmail.com>
2024-02-14 04:03:54 +0900

    ✨feat: コンポーネント・状態管理情報の追加

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
+import { useRecoilValue } from "recoil";
 import { CardBody } from "./CardBody";
+import { CardRadioChoco } from "./CardRadioChoco";
 import { CardShareButton } from "./CardShareButton";
+import { cardAuthorState, cardBodyState, cardChocoTypeState } from "@/states/cardContentsState";
 
 export const Card = () => {
+    const chocoType = useRecoilValue(cardChocoTypeState);
+    const author = useRecoilValue(cardAuthorState);
+    const body = useRecoilValue(cardBodyState);
 
     return (
         <div>
+            <CardRadioChoco />
             <CardBody />
-            <CardShareButton />
+            <CardShareButton choco={chocoType} author={author} body={body} />
         </div>
     )
 }
