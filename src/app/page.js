cdd5d1397a019a832f018e9afeb31f0f699f210b
hengin-eer <hirokix0608@gmail.com>
2024-02-13 23:38:33 +0900

    ✨feat: `Recoil`のatom参照が出来るようにプロバイダーを追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
+"use client";
+
+import { Card } from "@/components/Card";
 import { Description } from "@/components/Description";
 import Image from "next/image";
+import { RecoilRoot } from "recoil";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen px-5">
-      <Description />
-    </main>
+    <RecoilRoot>
+      <main className="flex flex-col items-center min-h-screen px-5">
+        <Description />
+        <Card />
+      </main>
+    </RecoilRoot>
   );
 }
