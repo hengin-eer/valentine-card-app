de09d6e986588fbad5dca50246c1b6a2014c90ac
hengin-eer <hirokix0608@gmail.com>
2024-02-14 04:03:18 +0900

    🐛fix: コンテンツ情報を3つの状態に分けて管理する

--- src/states/cardContentsState.js
+++ src/states/cardContentsState.js
@@ -1,6 +1,16 @@
 import { atom } from "recoil";
 
-export const cardContentsState = atom({
-    key: "cardContents",
+export const cardChocoTypeState = atom({
+    key: "cardChocoType",
+    default: "",
+});
+
+export const cardAuthorState = atom({
+    key: "cardAuthor",
+    default: "",
+});
+
+export const cardBodyState = atom({
+    key: "cardBody",
     default: "",
 });
