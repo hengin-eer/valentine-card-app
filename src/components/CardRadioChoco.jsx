87881907a549f154d7c6c04df16cd890f31ecee3
hengin-eer <hirokix0608@gmail.com>
2024-03-11 23:01:22 +0900

    ✨feat: `id`の変更による修正と、選択したチョコタイプごとにチョコ画像を表示させる

--- src/components/CardRadioChoco.jsx
+++ src/components/CardRadioChoco.jsx
@@ -5,19 +5,19 @@ export const CardRadioChoco = () => {
     const [cardChocoType, setCardChocoType] = useRecoilState(cardChocoTypeState);
     const options = [
         {
-            "id": "choco1",
+            "id": "1",
             "value": "🥰本命チョコ（勇気出して！）",
         },
         {
-            "id": "choco2",
+            "id": "2",
             "value": "🎈義理チョコ（「義理」です。）",
         },
         {
-            "id": "choco3",
+            "id": "3",
             "value": "✨友チョコ（おやつとして）",
         },
         {
-            "id": "choco4",
+            "id": "4",
             "value": "👩‍👧‍👦ファミチョコ（家族へ）",
         },
     ]
@@ -34,16 +34,17 @@ export const CardRadioChoco = () => {
             >
                 {options.map((item) => (
                     <div
+                        key={item.id}
                         className="flex items-center mb-4"
                     >
                         <input
                             className="w-5 h-5 mr-3 focus:ring-red-500 focus:ring-2"
-                            type="radio" id={item.id} value={item.value} name="kinda-choco"
+                            type="radio" id={`choco${item.id}`} value={item.value} name="kinda-choco"
                             onChange={(e) => updateChocoType(e)}
                         />
                         <label
                             className="text-lg"
-                            htmlFor={item.id}
+                            htmlFor={`choco${item.id}`}
                         >
                             {item.value}
                         </label>
@@ -51,8 +52,8 @@ export const CardRadioChoco = () => {
                 ))}
             </form>
             {/* chocoTypeの値ごとに表示するチョコレート画像を変更する */}
-            <div className="py-4">
-                <div className="h-[60px] aspect-square rounded-full bg-p-300"></div>
+            <div className="py-2">
+                <img src={(cardChocoType === "") ? "/cupid.png" : `/${cardChocoType}.png`} className="h-[150px]" alt="chocolate images" />
             </div>
         </div>
     )
